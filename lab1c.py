67d3f42f9f1c6dde55bbc355dae0cf24375cf0cc
Darcy-Mclaughlin <darcymclaughlin1234@gmail.com>
2026-09-16 19:57:08 +0000

    Completed Lab

--- lab1c.py
+++ lab1c.py
@@ -1,6 +1,3 @@
-
-# Add comments before you do anything else.
-
 #!/usr/bin/env python3
 # Author: Darcy McLaughlin 
 # Date: 16/09/26
@@ -9,8 +6,14 @@
 
 #TO-DO 1:
 # import math module.
-Import `math` module
+import math
 
 # Create a variable called 'radius' and take its value form user.
+radius = input ("Enter the Radius: ")
+
 # Convert the variable to integer using int()
+radius = int(radius)
+
 # use the contant pi form math module and compute the area of the circle using the variable 'radius'
+Area = (math.pi * (radius ** 2))
+print (Area)
